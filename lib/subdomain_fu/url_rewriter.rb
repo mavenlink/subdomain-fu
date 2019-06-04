c07025ba59aeeec44a2780ad0fc798bb1e145677
Greyson Richey <grichey@mavenlink.com>
2019-06-04 11:10:14 -0600

    Exchange usage of alias_method_chain with prepend
    Co-authored-by Brandon Duff <bduff@mavenlink.com>

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -1,17 +1,14 @@
 require 'action_dispatch/routing/route_set'
 
-module ActionDispatch
-  module Routing
-    class RouteSet #:nodoc:
-      def url_for_with_subdomains(options, *args)
-        if SubdomainFu.needs_rewrite?(options[:subdomain], options[:host]) || options[:only_path] == false
-          options[:only_path] = false if SubdomainFu.override_only_path?
-          options[:host] = SubdomainFu.rewrite_host_for_subdomains(options[:subdomain], options[:host])
-        end
-        options.delete(:subdomain) if (route_name = options[:use_route]) && (route = named_routes.get(route_name)) && route.requirements[:subdomain].blank?
-        url_for_without_subdomains(options, *args)
-      end
-      alias_method_chain :url_for, :subdomains
+module UrlForWithSubdomains
+  def url_for(options, *args)
+    if SubdomainFu.needs_rewrite?(options[:subdomain], options[:host]) || options[:only_path] == false
+      options[:only_path] = false if SubdomainFu.override_only_path?
+      options[:host] = SubdomainFu.rewrite_host_for_subdomains(options[:subdomain], options[:host])
     end
+    options.delete(:subdomain) if (route_name = options[:use_route]) && (route = named_routes.get(route_name)) && route.requirements[:subdomain].blank?
+    super(options, *args)
   end
 end
+
+ActionDispatch::Routing::RouteSet.prepend(UrlForWithSubdomains)
